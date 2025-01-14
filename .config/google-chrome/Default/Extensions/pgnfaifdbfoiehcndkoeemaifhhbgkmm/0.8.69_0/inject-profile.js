de16059f7d3637444c3cb6659e309e6f809e0799
Mikhail-Ramirez <mikhail.ramirez@outposttechnologies.com>
2025-01-14 08:09:54 -0600

    cleanup

--- .config/google-chrome/Default/Extensions/pgnfaifdbfoiehcndkoeemaifhhbgkmm/0.8.69_0/inject-profile.js
+++
@@ -1,9 +0,0 @@
-var profileData_entry_point = document.createElement("div");
-var reactJS_script = document.createElement("script");
-
-profileData_entry_point.id = "profileData";
-reactJS_script.src = "profileData.bundle.js";
-
-profileData_entry_point.appendChild(reactJS_script);
-document.querySelector("body").appendChild(profileData_entry_point);
-
--- .config/google-chrome/Default/Extensions/pgnfaifdbfoiehcndkoeemaifhhbgkmm/0.8.69_0/inject-template1.js
+++
@@ -1,6 +0,0 @@
-var popupTemplate_entry_point = document.createElement("div");
-var reactJS_script = document.createElement("script");
-popupTemplate_entry_point.id = "pauseTemplate";
-reactJS_script.src = "pauseTemplate.bundle.js";
-popupTemplate_entry_point.appendChild(reactJS_script);
-document.querySelector("body").appendChild(popupTemplate_entry_point);
